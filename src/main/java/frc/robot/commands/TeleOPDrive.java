8daaea5e1a85fec64fd464da07ca884a1b17a90a
Aidan Underwood <aidanunderwood1@icloud.com>
2022-01-08 17:50:00 -0600

    Changed XboxController to Joystick

--- src/main/java/frc/robot/commands/TeleOPDrive.java
+++ src/main/java/frc/robot/commands/TeleOPDrive.java
@@ -1,15 +1,15 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Drivetrain;
 
 public class TeleOPDrive extends CommandBase {
   private int translationAxis;
-  private XboxController driver;
+  private Joystick driver;
   private Drivetrain tankdrive;
 
-  public TeleOPDrive(Drivetrain tankdrive, XboxController controller, int translationAxis) {
+  public TeleOPDrive(Drivetrain tankdrive, Joystick controller, int translationAxis) {
     this.translationAxis = translationAxis;
     this.driver = controller;
     this.tankdrive = tankdrive;
