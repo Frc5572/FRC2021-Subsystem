63b02a17ef9193a86735d8a8cc53bbd4089a692a
aidan-underwood <61338453+aidan-underwood@users.noreply.github.com>
2022-01-09 12:05:31 -0600

    Installed REVColorSensor lib, created Constants

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,31 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.util.Color;
+import com.revrobotics.ColorMatch;
+
+public final class Constants {
+  final double x1 = -0.0000000025291;
+  final double x2 = 0.0000334240538;
+  final double x3 = -0.1545379987062;
+  final double b = 315.5170993015826;
+  final double heightOfShooter = 38;
+  final double heightOfTower = 98;
+  final double heightdiff = heightOfTower - heightOfShooter;
+  final double minAngle = 25;
+  final double maxAngle = 65;
+  final double maxPosition = 0;
+  final double minPosition = 1;
+  final double m1 = -(maxPosition - minPosition) / (maxAngle - minAngle);
+  final double b1 = -.625;
+  final double limitTurret = 20;
+  final double limitServo = .7;
+  final double hoodOffset = 30;
+  final double pi = 3.14159265358979323846;
+
+  private final ColorMatch m_colorMatcher = new ColorMatch();
+
+  private final Color kBlueTarget = ColorMatch.makeColor(0,0,1);
+  private final Color kGreenTarget = ColorMatch.makeColor(0,1,0);
+  private final Color kRedTarget = ColorMatch.makeColor(1,0,0);
+  private final Color kYellowTarget = ColorMatch.makeColor(.5, 1, 0);
+}
