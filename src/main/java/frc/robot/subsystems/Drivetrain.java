f0317eff9f0b263c15d50f951021eb5380573c09
Alex Resnick <adr8292@gmail.com>
2022-01-09 11:37:24 -0600

    ignore line length earning

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -5,8 +5,10 @@ import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Drivetrain extends SubsystemBase {
+  //CHECKSTYLE:OFF
   private final SpeedControllerGroup leftMotors = new SpeedControllerGroup(new WPI_TalonSRX(4), new WPI_TalonSRX(8));
   private final SpeedControllerGroup rightMotors = new SpeedControllerGroup(new WPI_TalonSRX(3), new WPI_TalonSRX(2));
+  //CHECKSTYLE:ON
 
   public Drivetrain() {
     rightMotors.setInverted(true);
