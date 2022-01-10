d6881c975254b70f3f69a5cb6074906c6c4fd859
Alex Resnick <adr8292@gmail.com>
2022-01-10 13:37:20 -0600

    test

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -2,9 +2,9 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot;
 
 import edu.wpi.first.wpilibj.RobotBase;
+package frc.robot;
 
 /**
  * Do NOT add any static variables to this class, or any initialization at all. Unless you know what
@@ -12,7 +12,10 @@ import edu.wpi.first.wpilibj.RobotBase;
  * call.
  */
 public final class Main {
-  private Main() {}
+  private Main() {
+  
+  
+  }
 
   /**
    * Main initialization function. Do not perform any initialization here.
@@ -20,6 +23,5 @@ public final class Main {
    * <p>If you change your main robot class, change the parameter type.
    */
   public static void main(String... args) {
-    RobotBase.startRobot(Robot::new);
-  }
+    RobotBase.startRobot(Robot::new);  }
 }
