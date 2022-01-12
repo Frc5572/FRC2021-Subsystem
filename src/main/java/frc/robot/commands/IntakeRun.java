9fe0dc1635b28dfce2f5860e08ad66a4b227de7b
Shyam <shyamdayal@sbcglobal.net>
2022-01-12 17:28:41 -0600

    add Intake command

--- src/main/java/frc/robot/commands/IntakeRun.java
+++ src/main/java/frc/robot/commands/IntakeRun.java
@@ -3,10 +3,29 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Intake;
 
+
 public class IntakeRun extends CommandBase {
 
-  // Intake sub;
+  private final Intake intake;
+  
+  public IntakeRun(Intake subsystem){
+   
 
-  // public Intakerun(Intake)
+    intake = subsystem;
+
+    
+    addRequirements(intake);
+
+  }
+
+  public void intakeOut(){
+    intake.intakeOut();
+  }
+
+  public void intakeIn(){
+    intake.intakeIn();
+  }
+
+  
 
 }
