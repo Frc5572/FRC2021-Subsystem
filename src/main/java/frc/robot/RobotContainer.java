5c8c044173733be9f82b6721163cefcbb081b708
owenflatman <74746499+owenflatman@users.noreply.github.com>
2022-01-13 17:59:18 -0600

    Add Climber Subsystem (#25)
    Co-authored-by: aidan-underwood <61338453+aidan-underwood@users.noreply.github.com>

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -2,6 +2,9 @@ package frc.robot;
 
 // import frc.robot.Autonomi.*;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.button.POVButton;
 import frc.robot.commands.*;
 import frc.robot.subsystems.*;
 
@@ -9,9 +12,20 @@ public class RobotContainer {
   Joystick driver = new Joystick(0);
   Joystick operator = new Joystick(1);
 
+  private final JoystickButton deploy = new JoystickButton(driver, XboxController.Button.kY.value);
+  private final POVButton rachetMotors = new POVButton(driver, 180);
   private final Drivetrain tankDrive = new Drivetrain();
+  private final Climber climb = new Climber();
 
   public RobotContainer() {
     tankDrive.setDefaultCommand(new TeleOPDrive(tankDrive, driver));
+    configureButtonBindings();
   }
+
+  private void configureButtonBindings() {
+    deploy.whenPressed(new DeployClimber(climb));
+    rachetMotors.whileHeld(new ExecuteClimber(climb));
+  }
+
+
 }
