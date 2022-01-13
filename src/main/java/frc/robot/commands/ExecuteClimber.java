d154ccc7ecf49a127132ff159a40b96fc02de1ac
owenflatman <owenjflatman@gmail.com>
2022-01-12 18:55:40 -0600

    done

--- src/main/java/frc/robot/commands/ExecuteClimber.java
+++ src/main/java/frc/robot/commands/ExecuteClimber.java
@@ -4,7 +4,7 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Climber;
 
 /**
-* Returns a Command to run the climber
+* Returns a Command to run the climber.
 */
 public class ExecuteClimber extends CommandBase {
   private Climber climb;
