73ae0d4e2149a6ec3cd6fb85dc4d2d51cffd2813
Shyam <shyamdayal@sbcglobal.net>
2022-01-13 17:54:32 -0600

    changed lines

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,9 +4,9 @@ package frc.robot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.commands.*;
-import frc.robot.subsystems.*;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.subsystems.*;
+
 
 public class RobotContainer {
   Joystick driver = new Joystick(0);
@@ -14,14 +14,15 @@ public class RobotContainer {
 
   private final Drivetrain tankDrive = new Drivetrain();
   private final Intake intake = new Intake();
-  private final JoystickButton intakeButton = new JoystickButton(operator, XboxController.Button.kB.value);
+  private final JoystickButton intakeButton = 
+    new JoystickButton(operator, XboxController.Button.kB.value);
 
   public RobotContainer() {
     tankDrive.setDefaultCommand(new TeleOPDrive(tankDrive, driver));
     configureButtonBindings();
   }
 
-  private void configureButtonBindings(){ 
+  private void configureButtonBindings() { 
     intakeButton.whileHeld(new IntakeRun(intake));
   }
 }
