8f9836e332498c6ed1fde474cf55b3571b743cc2
owenflatman <owenjflatman@gmail.com>
2022-01-13 17:54:26 -0600

    done done

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -3,7 +3,6 @@ package frc.robot;
 // import frc.robot.Autonomi.*;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.POVButton;
 import frc.robot.commands.*;
