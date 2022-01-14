4bb64e669167fa004e22f1b41a56c1954977f92e
Shyam <shyamdayal@sbcglobal.net>
2022-01-14 17:13:04 -0600

    fixed indentation

--- src/main/java/frc/robot/subsystems/Hopper.java
+++ src/main/java/frc/robot/subsystems/Hopper.java
@@ -2,42 +2,42 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
-import frc.robot.commands.*;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 
 
 /**
- *
+ *Sets up solonoid and hopper
  */
 public class Hopper extends SubsystemBase {
 
   int pcm1 = 0;
   int pcm2 = 1;
-  final SpeedControllerGroup hopper = new SpeedControllerGroup(new WPI_TalonSRX(9), new WPI_TalonSRX(10));
+  final SpeedControllerGroup hopper = 
+    new SpeedControllerGroup(new WPI_TalonSRX(9), new WPI_TalonSRX(10));
   DoubleSolenoid hopperSol = new DoubleSolenoid(pcm1, 5, 2);
 
 
-    public Hopper() {
+  public Hopper() {
         // Intializing Motors & Object
-    }
+      }
 
-    public void up() {
-      hopper.set(.5);
-      hopperSol.set(Value.kForward);
-
-    }
+  public void up() {
+    hopper.set(.5);
+    hopperSol.set(Value.kForward);
+  }
+  
     public void down() {
-      hopper.set(-.5);
-
-    }
+    hopper.set(-.5);
+  }
+  
     public void stop() {
-      hopper.set(0);
-      hopperSol.set(Value.kReverse);
-    }
+    hopper.set(0);
+    hopperSol.set(Value.kReverse);
+  }
 
 
 }
