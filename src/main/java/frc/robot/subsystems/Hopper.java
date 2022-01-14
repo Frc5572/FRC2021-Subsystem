3b90bc8a5eb4d09f1b7e734649233ad72e56dfd9
Shyam <shyamdayal@sbcglobal.net>
2022-01-14 17:17:28 -0600

    fixed indentation

--- src/main/java/frc/robot/subsystems/Hopper.java
+++ src/main/java/frc/robot/subsystems/Hopper.java
@@ -4,37 +4,37 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 
 /**
- *Sets up solonoid and hopper
+ *Sets up solonoid and hopper.
  */
 public class Hopper extends SubsystemBase {
 
   int pcm1 = 0;
   int pcm2 = 1;
   final SpeedControllerGroup hopper = 
-    new SpeedControllerGroup(new WPI_TalonSRX(9), new WPI_TalonSRX(10));
+      new SpeedControllerGroup(new WPI_TalonSRX(9), new WPI_TalonSRX(10));
   DoubleSolenoid hopperSol = new DoubleSolenoid(pcm1, 5, 2);
 
 
   public Hopper() {
-        // Intializing Motors & Object
-      }
+        // Intializing Motors & Object.
+  }
 
   public void up() {
     hopper.set(.5);
     hopperSol.set(Value.kForward);
   }
   
-    public void down() {
+  public void down() {
     hopper.set(-.5);
   }
   
-    public void stop() {
+  public void stop() {
     hopper.set(0);
     hopperSol.set(Value.kReverse);
   }
