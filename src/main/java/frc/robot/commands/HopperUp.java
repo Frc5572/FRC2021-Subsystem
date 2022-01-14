bf4670e111b59b747033147354990d6f2ca4caac
Shyam <shyamdayal@sbcglobal.net>
2022-01-14 17:19:56 -0600

    fixed indentation

--- src/main/java/frc/robot/commands/HopperUp.java
+++ src/main/java/frc/robot/commands/HopperUp.java
@@ -2,10 +2,12 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Hopper;
+
 /** 
-*Creates Hopper up command which allows to hopper to move up.
+* Creates Hopper up command which allows to hopper to move up.
 */
 public class HopperUp extends CommandBase {
+  
   private Hopper hopper;
 
   public HopperUp(Hopper hopper) {
