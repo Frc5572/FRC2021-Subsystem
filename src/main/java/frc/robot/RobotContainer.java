06f9544741906123da0beaa2e0ba8596416b94c1
Shyam <shyamdayal@sbcglobal.net>
2022-01-13 18:53:49 -0600

    removed extra lines

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,7 +8,6 @@ import edu.wpi.first.wpilibj2.command.button.POVButton;
 import frc.robot.commands.*;
 import frc.robot.subsystems.*;
 
-
 public class RobotContainer {
   Joystick driver = new Joystick(0);
   Joystick operator = new Joystick(1);
@@ -20,6 +19,7 @@ public class RobotContainer {
   private final JoystickButton intakeButton = 
         new JoystickButton(operator, XboxController.Button.kB.value);
   private final Climber climb = new Climber();
+  
   public RobotContainer() {
     tankDrive.setDefaultCommand(new TeleOPDrive(tankDrive, driver));
     configureButtonBindings();
