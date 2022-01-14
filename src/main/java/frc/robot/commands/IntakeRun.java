bf2d70c1c1428608b552cbfd5dd689dfb38ad9fe
Shyam <shyamdayal@sbcglobal.net>
2022-01-13 18:34:31 -0600

    asd

--- src/main/java/frc/robot/commands/IntakeRun.java
+++ src/main/java/frc/robot/commands/IntakeRun.java
@@ -3,14 +3,12 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Intake;
 
-/*Yes
-Maybe
-No*/
+/*
+* THis generates a command to move the intake out.
+*/
 public class IntakeRun extends CommandBase {
   private final Intake intake;
-  /*Yes
-  Maybe
-  No*/
+
   public IntakeRun(Intake subsystem) {
     this.intake = subsystem;
     addRequirements(intake);
@@ -21,11 +19,9 @@ public class IntakeRun extends CommandBase {
     intake.intakeOut();
   }
 
-  
   @Override
   public void end(boolean interruptible) {
     intake.intakeIn();
   }
-  
 
 }
