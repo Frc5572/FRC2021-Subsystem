435a7fc8727603413a63c6f48d35cf22e35abe01
Aidan Underwood <aidanunderwood1@icloud.com>
2022-01-18 12:50:55 -0600

    Beginning commit, start on framework

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -3,15 +3,25 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.math.*;
 
 public class Drivetrain extends SubsystemBase {
   private final SpeedControllerGroup leftMotors =
-      new SpeedControllerGroup(new WPI_TalonSRX(4), new WPI_TalonSRX(8));
+    new SpeedControllerGroup(new WPI_TalonSRX(4), new WPI_TalonSRX(8));
   private final SpeedControllerGroup rightMotors =
-      new SpeedControllerGroup(new WPI_TalonSRX(3), new WPI_TalonSRX(2));
+    new SpeedControllerGroup(new WPI_TalonSRX(3), new WPI_TalonSRX(2));
+  private final Encoder leftEncoder =
+    new Encoder(4, 8);
+  private final Encoder rightEncoder =
+    new Encoder(3, 2);
 
   public Drivetrain() {
     rightMotors.setInverted(true);
+
+    leftEncoder.setDistancePerPulse(Constants.kEncoderDistancePerPulse);
+    rightEncoder.setDistancePerPulse(Constants.kEncoderDistancePerPulse);
   }
 
   public void tankDrive(double yAxis, double yAxis2) {
