d368403239bbe3ab18bbe1ef421359c934fd9247
Alex Grinman <90881143+gvaldez7206@users.noreply.github.com>
2022-01-19 18:18:16 -0600

    making even more changes

--- src/main/java/frc/robot/commands/TeleOPDrive.java
+++ src/main/java/frc/robot/commands/TeleOPDrive.java
@@ -6,7 +6,7 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Drivetrain;
 
 /**
- * I don't know what it creates 
+ * I don't know what it creates.
  */
 public class TeleOPDrive extends CommandBase {
   private Joystick driver;
